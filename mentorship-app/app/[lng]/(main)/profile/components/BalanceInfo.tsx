3c9ed5af10ccc9fbc4bce6aee6f611558795e3fe
TeraBasedProgrammer <ilyadronov2332@gmail.com>
2025-04-24 13:49:11 +0300

    feat: add payment components

--- mentorship-app/app/[lng]/(main)/profile/components/BalanceInfo.tsx
+++ mentorship-app/app/[lng]/(main)/profile/components/BalanceInfo.tsx
@@ -1,55 +1,11 @@
 'use client';
 
+import { UserData } from '@/app/types';
 import { Card, CardContent } from '@/components/ui/card';
-import { toast } from '@/hooks/useToast';
-import { axiosInstance } from '@/lib/services/axiosConfig';
-import { useSession } from 'next-auth/react';
-import { useState } from 'react';
 import { PiCoinVerticalFill } from 'react-icons/pi';
 import CreditsChoiceDialog from './CreditsChoiceDialog';
 
-const BalanceInfo = () => {
-  const { data: session, update: updateSession } = useSession();
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleIncreaseBalance = async () => {
-    setIsLoading(true);
-    try {
-      const response = await axiosInstance.post(
-        `/users/${session?.user?.id}/increase-balance`,
-        { amount: 100 }, // You can make this configurable
-        {
-          headers: {
-            Authorization: `Bearer ${session?.accessToken}`,
-          },
-        },
-      );
-
-      // Update session with new balance
-      await updateSession({
-        ...session,
-        user: {
-          ...session?.user,
-          balance: response.data.balance,
-        },
-      });
-
-      toast({
-        title: 'Success',
-        description: 'Balance increased successfully',
-        variant: 'success',
-      });
-    } catch (error) {
-      toast({
-        title: 'Error',
-        description: 'Failed to increase balance',
-        variant: 'destructive',
-      });
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
+const BalanceInfo = ({ userData }: { userData: UserData | null }) => {
   return (
     <>
       <h2 className="text-4xl max-w-3xl mx-auto mb-8">Balance info</h2>
@@ -59,7 +15,7 @@ const BalanceInfo = () => {
             <div>
               <p className="text-sm text-muted-foreground mb-2">Current Balance</p>
               <p className="text-2xl font-bold flex items-center gap-1">
-                {session?.user?.balance || 0} <PiCoinVerticalFill />{' '}
+                {userData?.balance || 0} <PiCoinVerticalFill />{' '}
               </p>
             </div>
             <CreditsChoiceDialog />
--- mentorship-app/app/[lng]/(main)/profile/components/CreditsChoiceDialog.tsx
+++ mentorship-app/app/[lng]/(main)/profile/components/CreditsChoiceDialog.tsx
@@ -7,44 +7,112 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog';
+import { toast } from '@/hooks/useToast';
+import { axiosInstance } from '@/lib/services/axiosConfig';
+import { loadStripe } from '@stripe/stripe-js';
+import { AxiosError } from 'axios';
+import { useSession } from 'next-auth/react';
+import { useState } from 'react';
 
 const creditsVariants = [
   {
     title: '25 credits',
     description: 'Enough credits to pay for 1-2 lessons. Suitable for beginner learners.',
-    price: 10,
+    amount: 25,
+    price: 4,
   },
   {
     title: '200 credits',
     description: 'Enough credits to pay for 10-12 lessons. Suitable for intermediate learners.',
-    price: 10,
+    amount: 200,
+    price: 30,
   },
   {
     title: '500 credits',
     description: 'Enough credits to pay for 25-30 lessons. Suitable for advanced learners.',
-    price: 10,
+    amount: 500,
+    price: 70,
   },
 ];
 
 export default function CreditsChoiceDialog() {
+  const [isLoading, setIsLoading] = useState<number | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const { data: session } = useSession();
+
+  const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+
+  const handlePurchase = async (variant: (typeof creditsVariants)[number]) => {
+    setIsLoading(variant.amount);
+    try {
+      const stripe = await stripePromise;
+      const stripeSessionResponse = await axiosInstance.post(
+        '/billing/create-checkout-session',
+        {
+          credits_amount: variant.amount,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${session?.accessToken}`,
+          },
+        },
+      );
+
+      const error = await stripe?.redirectToCheckout({
+        sessionId: stripeSessionResponse.data.session_id,
+      });
+      if (error) {
+        toast({
+          title: 'Purchase failed',
+          description: 'Please try again',
+          variant: 'destructive',
+        });
+      }
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        toast({
+          title: 'Error',
+          description: `${error.response?.data.detail}`,
+          variant: 'destructive',
+        });
+      } else {
+        toast({
+          title: 'Error',
+          description: `Failed to purchase credits: ${error}`,
+          variant: 'destructive',
+        });
+      }
+    } finally {
+      setIsLoading(null);
+    }
+  };
+
   return (
-    <Dialog>
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogTrigger asChild>
         <Button className="bg-blue-brand text-white">Increase Balance</Button>
       </DialogTrigger>
-      <DialogContent className="w-2/3">
+      <DialogContent className="w-3xl max-w-full mx-auto">
         <DialogHeader>
           <DialogTitle className="text-center">Choose credits amount</DialogTitle>
         </DialogHeader>
-        <div className="flex justify-center items-center gap-4 w-3xl">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {creditsVariants.map((variant) => (
-            <Card key={variant.title} className="w-1/3 h-full">
+            <Card key={variant.title} className="h-full">
               <CardHeader>
                 <CardTitle>{variant.title}</CardTitle>
-                <CardDescription className="">{variant.description}</CardDescription>
+                <CardDescription className="min-h-[60px]">{variant.description}</CardDescription>
+                <p className="text-xl font-bold text-center">${variant.price}</p>
               </CardHeader>
               <CardFooter className="flex justify-center items-center">
-                <Button className="bg-blue-brand text-white">Buy</Button>
+                <Button
+                  className="bg-blue-brand text-white w-full"
+                  onClick={() => handlePurchase(variant)}
+                  disabled={isLoading === variant.amount}>
+                  {isLoading === variant.amount ? 'Processing...' : 'Buy'}
+                </Button>
               </CardFooter>
             </Card>
           ))}
